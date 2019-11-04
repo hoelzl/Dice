16cb611c4c0b6a1b6b6feba659eb2e0033cdb65e
Matthias Hölzl <tc@xantira.com>
2019-11-04 16:37:53 +0100

    Remove implementation files

--- src/test/java/de/mhoelzl/dice/random/SimpleRngTest.java
+++
@@ -1,8 +0,0 @@
-package de.mhoelzl.dice.random;
-
-class SimpleRngTest extends RandomNumberGeneratorTest {
-    @Override
-    RandomNumberGenerator getGenerator() {
-        return new SimpleRng(6);
-    }
-}
